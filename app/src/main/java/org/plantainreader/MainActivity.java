39ad06056f4952eb42bfc3a813f37b1c005fe92e
krikunts <krikunts@gmail.com>
2017-04-06 21:57:10 +0300

    enable nfc dialog

--- app/src/main/java/org/plantainreader/MainActivity.java
+++ app/src/main/java/org/plantainreader/MainActivity.java
@@ -1,15 +1,20 @@
 package org.plantainreader;
 
+import android.annotation.SuppressLint;
 import android.app.PendingIntent;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.nfc.NfcAdapter;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private NfcAdapter mNfcAdapter;
     private PendingIntent mPendingIntent;
+    private AlertDialog mEnableNfc;
 
 
     public void onCreate(Bundle savedInstanceState) {
@@ -34,6 +39,9 @@ public class MainActivity extends AppCompatActivity {
     public void initAdapter() {
         TextView statusText = (TextView) findViewById(R.id.status_text);
         if (this.isAdapterEnabled()) {
+            if(mEnableNfc != null) {
+                mEnableNfc.hide();
+            }
             statusText.setText(getString(R.string.status_wait));
             if (!getPackageManager().hasSystemFeature("com.nxp.mifare")) {
                 statusText.append("\n\n" + getString(R.string.compat_warning));
@@ -45,7 +53,33 @@ public class MainActivity extends AppCompatActivity {
             }
         } else {
             statusText.setText(getString(R.string.nfc_disabled));
+            if(mEnableNfc == null) {
+                enableNfcDialog();
+            }
+            mEnableNfc.show();
         }
+    }
+
+    private void enableNfcDialog() {
+        mEnableNfc = new AlertDialog.Builder(this).setTitle(R.string.nfc_dialog_title)
+                .setMessage(R.string.nfc_dialog_message)
+                .setPositiveButton(R.string.nfc_dialog_yes,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            @SuppressLint("InlinedApi")
+                            public void onClick(DialogInterface dialog, int which) {
+                                // Goto NFC Settings.
+                                startActivity(new Intent(Settings.ACTION_NFC_SETTINGS));
+                            }
+                        })
+                .setNegativeButton(R.string.nfc_dialog_no,
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int id) {
+                                // Exit the App.
+                                finish();
+                            }
+                }).create();
     }
 
     protected void onPause() {
